fa79b619607821c32da77fa75d58276278bcafb0
SonOfEscobar1337 <97580283+sonofescobar1337@users.noreply.github.com>
2023-07-15 15:06:04 +0000

    update the thread pool

--- ip2domain.py
+++ ip2domain.py
@@ -1,4 +1,13 @@
 import sys,os,re,socket,binascii,time,json,random,threading,Queue,pprint,urlparse,smtplib,telnetlib,os.path,hashlib,string,urllib2,glob,sqlite3,urllib,argparse,marshal,base64,colorama,requests
+import sys
+import os
+import re
+import socket
+import binascii
+import time
+import random
+import threading
+from multiprocessing.pool import ThreadPool
 from colorama import *
 from random import choice
 from colorama import Fore,Back,init
@@ -11,25 +20,11 @@ from urlparse import urlparse
 from urllib2 import urlopen
 colorama.init()
 
-
-# Now regular ANSI codes should work, even in Windows
-CLEAR_SCREEN = '\033[2J'
-RED = '\033[31m'   # mode 31 = red forground
-RESET = '\033[0m'  # mode 0  = reset
-BLUE  = "\033[34m"
-CYAN  = "\033[36m"
-GREEN = "\033[32m"
-RESET = "\033[0m"
-BOLD    = "\033[m"
-REVERSE = "\033[m"
-#coded by mister spy
-#contact me 712083179
 def logo():
-        clear = "\x1b[0m"
-        colors = [36, 32, 34, 35, 31, 37  ]
+    clear = "\x1b[0m"
+    colors = [36, 32, 34, 35, 31, 37]
 
-        x = """ 
-
+    x = """ 
   ____                        _       ___                       _     _               
  |  _ \  ___  _ __ ___   __ _(_)_ __ |_ _|_ __   __ _ _ __ __ _| |__ | |__   ___ _ __ 
  | | | |/ _ \| '_ ` _ \ / _` | | '_ \ | || '_ \ / _` | '__/ _` | '_ \| '_ \ / _ \ '__|
@@ -39,32 +34,34 @@ def logo():
 				  
                   telegram group : https://t.me/linuxploit_com
 			                  """
-        for N, line in enumerate(x.split("\n")):
-            sys.stdout.write("\x1b[1;%dm%s%s\n" % (random.choice(colors), line, clear))
-            time.sleep(0.05)
-logo()
-
+    for N, line in enumerate(x.split("\n")):
+        sys.stdout.write("\x1b[1;%dm%s%s\n" % (random.choice(colors), line, clear))
+        time.sleep(0.05)
 
 def getIP(site):
-	
-		site = i.strip()
-		try:
-			if 'http://' not in site:
-				IP1 = socket.gethostbyname(site)
-				print "IP: "+IP1
-				open('ips.txt', 'a').write(IP1+'\n')
-			elif 'http://' in site:
-				url = site.replace('http://', '').replace('https://', '').replace('/', '')
-				IP2 = socket.gethostbyname(url)
-				print "IP: "+IP2
-				open('ips.txt', 'a').write(IP2+'\n')
-	
-		except:
-			pass
-			
-nam=raw_input('Domain List name :')
-with open(nam) as f:
-    for i in f:
-        getIP(i)
+    try:
+        site = site.strip()
+        if 'http://' not in site:
+            IP1 = socket.gethostbyname(site)
+            print("IP: " + IP1)
+            with open('ips.txt', 'a') as f:
+                f.write(IP1 + '\n')
+        elif 'http://' in site:
+            url = site.replace('http://', '').replace('https://', '').replace('/', '')
+            IP2 = socket.gethostbyname(url)
+            print("IP: " + IP2)
+            with open('ips.txt', 'a') as f:
+                f.write(IP2 + '\n')
+    except:
+        pass
 
-		
+if __name__ == '__main__':
+    logo()
+    nam = raw_input('Domain List name: ') 
+    with open(nam) as f:
+        domain_list = f.read().splitlines()
+
+    pool = ThreadPool(1100) 
+    pool.map(getIP, domain_list)
+    pool.close()
+    pool.join()
